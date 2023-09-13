4d3226aa62589b62078fa7ccc8c941597e391252
chanel50 <106768673+Chanel50@users.noreply.github.com>
2023-09-13 23:13:53 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-/* This is a file inserted to keep the learn IDE browser happy */
+document.addEventListener("DOMContentLoaded", () => {
+    // Step 1: Uncomment this code to select like buttons
+    const likeButtons = document.querySelectorAll(".like-button");
+
+    // Step 2: Uncomment this code to simulate server communication
+    function mockServerCall() {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve({ status: "success" });
+            }, 1000);
+        });
+    }
+
+    // Step 3: Uncomment this code to add event listeners
+    likeButtons.forEach((button) => {
+        button.addEventListener("click", async () => {
+            // Simulate server communication
+            const response = await mockServerCall();
+
+            if (response.status === "success") {
+                // Toggle the like button
+                button.classList.toggle("liked");
+            }
+        });
+    });
+});
+
 const testVar = {}
